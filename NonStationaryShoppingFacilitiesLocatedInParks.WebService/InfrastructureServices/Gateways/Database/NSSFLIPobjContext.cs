4d4d8096cfa46b3944dd95bfe612cc830132f06b
Daniil <51130836+Daniilblag@users.noreply.github.com>
2020-06-01 15:50:48 +0900

    Add files via upload

---
+++ NonStationaryShoppingFacilitiesLocatedInParks.WebService/InfrastructureServices/Gateways/Database/NSSFLIPobjContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSSFLIPobj.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSSFLIPobj.InfrastructureServices.Gateways.Database
+{
+    public class NSSFLIPobjContext : DbContext
+    {
+        public DbSet<DomainObjects.NSSFLIPobj> NSSFLIPobjs { get; set; }
+
+
+        public NSSFLIPobjContext(DbContextOptions options)
+            : base(options)
+        { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DomainObjects.NSSFLIPobj>().HasData(
+              new DomainObjects.NSSFLIPobj()
+              {
+                  Id = 1L,
+                  Number = "1",
+                  NameObj = "№1, Нижний майский пруд между Майским просеком и Богородским шоссе",
+                  AdminArea = "Восточный административный округ",
+                  District = "Район Сокольники",
+                  Adress = "город Москва, улица Сокольнический Вал, дом 1, строение 1",
+                  CodAdrIP = "2109201",
+                  AreaObj = "285",
+                  KindObj = "павильон",
+                  Specialization = "кафе",
+                  PlacementPeriod = "с 1 января по 31 декабря",
+                  TypeOfContract = "договор на осущевствление торговой деятельности",
+                  ContractStatus = "действует",
+                  NumberOfContract = "592/15-A",
+                  NameOfBusinessEntity = "Ветров Михаил Владимирович",
+                  ContractStartDate = "30.06.2015",
+                  ContractFinishDate = "30.06.2020",
+                  GroundsForConcluding = "Результат аукциона (победитель)",
+                  TradingStartDate = "30.06.2015"
+              }
+           );
+        }      
+    }
+}
---
+++ NonStationaryShoppingFacilitiesLocatedInParks.WebService/InfrastructureServices/Gateways/Database/NSSFLIPobjEFSqliteGateway.cs
@@ -0,0 +1,49 @@
+﻿using NSSFLIPobj.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+using NSSFLIPobj.ApplicationServices.Ports.Gateways.Database;
+
+namespace NSSFLIPobj.InfrastructureServices.Gateways.Database
+{
+    public class NSSFLIPobjEFSqliteGateway : INSSFLIPobjDatabaseGateway
+    {
+        private readonly NSSFLIPobjContext _nssflipObjContext;
+
+        public NSSFLIPobjEFSqliteGateway(NSSFLIPobjContext nssflipObjContext)
+            => _nssflipObjContext = nssflipObjContext;
+
+        public async Task<DomainObjects.NSSFLIPobj> GetNSSFLIPobj(long id)
+           => await _nssflipObjContext.NSSFLIPobjs.FirstOrDefaultAsync();
+
+        public async Task<IEnumerable<DomainObjects.NSSFLIPobj>> GetAllNSSFLIPobj()
+            => await _nssflipObjContext.NSSFLIPobjs.ToListAsync();
+          
+        public async Task<IEnumerable<DomainObjects.NSSFLIPobj>> QueryNSSFLIPobj(Expression<Func<DomainObjects.NSSFLIPobj, bool>> filter)
+            => await _nssflipObjContext.NSSFLIPobjs.Where(filter).ToListAsync();
+
+        public async Task AddNSSFLIPobj(DomainObjects.NSSFLIPobj nssflipObj)
+        {
+            _nssflipObjContext.NSSFLIPobjs.Add(nssflipObj);
+            await _nssflipObjContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateNSSFLIPobj(DomainObjects.NSSFLIPobj nssflipobj)
+        {
+            _nssflipObjContext.Entry(nssflipobj).State = EntityState.Modified;
+            await _nssflipObjContext.SaveChangesAsync();
+        }
+
+        public async Task RemoveNSSFLIPobj(DomainObjects.NSSFLIPobj nssflipobj)
+        {
+            _nssflipObjContext.NSSFLIPobjs.Remove(nssflipobj);
+            await _nssflipObjContext.SaveChangesAsync();
+        }
+
+
+       
+    }
+}
