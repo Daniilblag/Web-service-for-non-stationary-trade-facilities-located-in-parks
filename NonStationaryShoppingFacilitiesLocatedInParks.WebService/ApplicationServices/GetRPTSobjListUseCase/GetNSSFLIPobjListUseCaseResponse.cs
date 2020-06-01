25fb629fc5f31e1ac69432a0287b3b05b80b2237
Daniil <51130836+Daniilblag@users.noreply.github.com>
2020-06-01 15:37:32 +0900

    Add files via upload

---
+++ NonStationaryShoppingFacilitiesLocatedInParks.WebService/ApplicationServices/GetRPTSobjListUseCase/GetNSSFLIPobjListUseCaseResponse.cs
@@ -0,0 +1,15 @@
+﻿using NSSFLIPobj.DomainObjects;
+using NSSFLIPobj.ApplicationServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSSFLIPobj.ApplicationServices.GetNSSFLIPobjListUseCase
+{
+    public class GetNSSFLIPobjListUseCaseResponse : UseCaseResponse
+    {
+        public IEnumerable<DomainObjects.NSSFLIPobj> NSSFLIPobj { get; }
+
+        public GetNSSFLIPobjListUseCaseResponse(IEnumerable<DomainObjects.NSSFLIPobj> nssflipObj) => NSSFLIPobj = nssflipObj;
+    }
+}
