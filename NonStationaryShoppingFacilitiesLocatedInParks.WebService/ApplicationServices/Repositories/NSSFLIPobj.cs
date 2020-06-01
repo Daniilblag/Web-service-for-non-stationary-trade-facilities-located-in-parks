73717e51a881319da41768f841e6a0fd5c5a066c
Daniil <51130836+Daniilblag@users.noreply.github.com>
2020-06-01 15:44:30 +0900

    Add files via upload

---
+++ NonStationaryShoppingFacilitiesLocatedInParks.WebService/ApplicationServices/Repositories/NSSFLIPobj.cs
@@ -0,0 +1,23 @@
+﻿using NSSFLIPobj.ApplicationServices.GetNSSFLIPobjListUseCase;
+using System.Net;
+using Newtonsoft.Json;
+using NSSFLIPobj.ApplicationServices.Ports;
+
+namespace NSSFLIPobj.InfrastructureServices.Presenters
+{
+    public class NSSFLIPobjListPresenter : IOutputPort<GetNSSFLIPobjListUseCaseResponse>
+    {
+        public JsonContentResult ContentResult { get; }
+
+        public NSSFLIPobjListPresenter()
+        {
+            ContentResult = new JsonContentResult();
+        }
+
+        public void Handle(GetNSSFLIPobjListUseCaseResponse response)
+        {
+            ContentResult.StatusCode = (int)(response.Success ? HttpStatusCode.OK : HttpStatusCode.NotFound);
+            ContentResult.Content = response.Success ? JsonConvert.SerializeObject(response.NSSFLIPobj) : JsonConvert.SerializeObject(response.Message);
+        }
+    }
+}
