162bae81ef2acd88c5d215db25495d81de955121
Daniil <51130836+Daniilblag@users.noreply.github.com>
2020-06-01 15:18:56 +0900

    Add files via upload

---
+++ NonStationaryShoppingFacilitiesLocatedInParks.WebService/DomainObjects/Ports/INSSFLIPobjRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace NSSFLIPobj.DomainObjects.Ports
+{
+    public interface IReadOnlyNSSFLIPobjRepository
+    {
+        Task<NSSFLIPobj> GetNSSFLIPobj(long id);
+
+        Task<IEnumerable<NSSFLIPobj>> GetAllNSSFLIPobjs();
+
+        Task<IEnumerable<NSSFLIPobj>> QueryNSSFLIPobj(ICriteria<NSSFLIPobj> criteria);
+
+    }
+
+    public interface INSSFLIPobjRepository
+    {
+        Task AddNSSFLIPobj(NSSFLIPobj nssflipObj);
+
+        Task RemoveNSSFLIPobj(NSSFLIPobj nssflipObj);
+
+        Task UpdateNSSFLIPobj(NSSFLIPobj nssflipObj);
+    }
+}
