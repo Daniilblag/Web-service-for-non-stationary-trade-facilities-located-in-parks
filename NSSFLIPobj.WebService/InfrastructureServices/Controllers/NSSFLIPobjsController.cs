ce87c5aa4e6f2b68cbf4823a321a4c688bc5a7b0
Daniil <51130836+Daniilblag@users.noreply.github.com>
2020-06-02 14:41:17 +0900

    Add files via upload

---
+++ NSSFLIPobj.WebService/InfrastructureServices/Controllers/NSSFLIPobjsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NSSFLIPobj.DomainObjects;
+using NSSFLIPobj.ApplicationServices.GetDistrictListUseCase;
+using NSSFLIPobj.InfrastructureServices.Presenters;
+
+namespace NSSFLIPobj.InfrastructureServices.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NSSFLIPobjsController : ControllerBase
+    {
+        private readonly ILogger<NSSFLIPobjsController> _logger;
+        private readonly IGetNSSFLIPobjListUseCase _getNSSFLIPobjListUseCase;
+
+        public NSSFLIPobjsController(ILogger<NSSFLIPobjsController> logger,
+                                IGetNSSFLIPobjListUseCase getNSSFLIPobjListUseCase)
+        {
+            _logger = logger;
+            _getNSSFLIPobjListUseCase = getNSSFLIPobjListUseCase;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllNSSFLIPobjs()
+        {
+            var presenter = new NSSFLIPobjListPresenter();
+            await _getNSSFLIPobjListUseCase.Handle(GetNSSFLIPobjListUseCaseRequest.CreateAllNSSFLIPobjsRequest(), presenter);
+            return presenter.ContentResult;
+        }
+
+        [HttpGet("{nssflipobjId}")]
+        public async Task<ActionResult> GetNSSFLIPobj(long nssflipobjId)
+        {
+            var presenter = new NSSFLIPobjListPresenter();
+            await _getNSSFLIPobjListUseCase.Handle(GetNSSFLIPobjListUseCaseRequest.CreateNSSFLIPobjRequest(nssflipobjId), presenter);
+            return presenter.ContentResult;
+        }
+
+        [HttpGet("metroline/{metroline}")]
+        public async Task<ActionResult> GetDistrictNSSFLIPobjs(string metroline)
+        {
+            var presenter = new NSSFLIPobjListPresenter();
+            await _getNSSFLIPobjListUseCase.Handle(GetNSSFLIPobjListUseCaseRequest.CreateNSSFLIPobjsRequest(metroline), presenter);
+            return presenter.ContentResult;
+        }
+    }
+}
