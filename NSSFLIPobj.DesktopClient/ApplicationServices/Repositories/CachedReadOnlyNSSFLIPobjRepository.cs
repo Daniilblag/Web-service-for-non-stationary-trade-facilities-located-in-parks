60759bec62af4747c76f8d6bb12dfb094d1feaa6
Daniil <51130836+Daniilblag@users.noreply.github.com>
2020-06-02 15:08:45 +0900

    Add files via upload

---
+++ NSSFLIPobj.DesktopClient/ApplicationServices/Repositories/CachedReadOnlyNSSFLIPobjRepository.cs
@@ -0,0 +1,33 @@
+﻿using NSSFLIPobj.ApplicationServices.Ports.Cache;
+using NSSFLIPobj.DomainObjects;
+using NSSFLIPobj.DomainObjects.Ports;
+using NSSFLIPobj.DomainObjects.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSSFLIPobj.ApplicationServices.Repositories
+{
+    public class CachedReadOnlyNSSFLIPobjRepository : ReadOnlyNSSFLIPobjRepositoryDecorator
+    {
+        private readonly IDomainObjectsCache<nssflipobj> _nssflipobjsCache;
+
+        public CachedReadOnlyNSSFLIPobjRepository(IReadOnlyNSSFLIPobjRepository nssflipobjRepository, 
+                                             IDomainObjectsCache<nssflipobj> nssflipobjsCache)
+            : base(nssflipobjRepository)
+            => _nssflipobjsCache = nssflipobjsCache;
+
+        public async override Task<nssflipobj> GetNSSFLIPobj(long id)
+            => _nssflipobjsCache.GetObject(id) ?? await base.GetNSSFLIPobj(id);
+
+        public async override Task<IEnumerable<nssflipobj>> GetAllNSSFLIPobjs()
+            => _nssflipobjsCache.GetObjects() ?? await base.GetAllNSSFLIPobjs();
+
+        public async override Task<IEnumerable<nssflipobj>> QueryNSSFLIPobjs(ICriteria<nssflipobj> criteria)
+            => _nssflipobjsCache.GetObjects()?.Where(criteria.Filter.Compile()) ?? await base.QueryNSSFLIPobjs(criteria);
+
+    }
+}
