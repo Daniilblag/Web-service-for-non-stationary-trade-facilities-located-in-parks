ad04e8a53ae329a1e2219299c768ec214bcc94e4
Daniil <51130836+Daniilblag@users.noreply.github.com>
2020-06-02 14:52:07 +0900

    Add files via upload

---
+++ NSSFLIPobj.GeneralLogic/ApplicationServices/Ports/Gateways/Database/INSSFLIPobjDatabaseGateway.cs
@@ -0,0 +1,25 @@
+﻿using NSSFLIPobj.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSSFLIPobj.ApplicationServices.Ports.Gateways.Database
+{
+    public interface INSSFLIPobjDatabaseGateway
+    {
+        Task AddNSSFLIPobj(nssflipobj nssflipobj);
+
+        Task RemoveNSSFLIPobj(nssflipobj nssflipobj);
+
+        Task UpdateNSSFLIPobj(nssflipobj nssflipobj);
+
+        Task<nssflipobj> GetNSSFLIPobj(long id);
+
+        Task<IEnumerable<nssflipobj>> GetAllNSSFLIPobjs();
+
+        Task<IEnumerable<nssflipobj>> QueryNSSFLIPobjs(Expression<Func<nssflipobj, bool>> filter);
+
+    }
+}
