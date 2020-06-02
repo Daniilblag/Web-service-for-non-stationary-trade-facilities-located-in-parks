f878c346a50fc0cc6d66139aa3b2fb62341cdf8a
Daniil <51130836+Daniilblag@users.noreply.github.com>
2020-06-02 14:54:49 +0900

    Add files via upload

---
+++ NSSFLIPobj.GeneralLogic/DomainObjects/Ports/INSSFLIPobjRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace NSSFLIPobj.DomainObjects.Ports
+{
+    public interface IReadOnlyNSSFLIPobjRepository
+    {
+        Task<nssflipobj> GetNSSFLIPobj(long id);
+
+        Task<IEnumerable<nssflipobj>> GetAllNSSFLIPobjs();
+
+        Task<IEnumerable<nssflipobj>> QueryNSSFLIPobjs(ICriteria<nssflipobj> criteria);
+
+    }
+
+    public interface INSSFLIPobjRepository
+    {
+        Task AddNSSFLIPobj(nssflipobj nssflipobj);
+
+        Task RemoveNSSFLIPobj(nssflipobj nssflipobj);
+
+        Task UpdateNSSFLIPobj(nssflipobj nssflipobj);
+    }
+}
---
+++ NSSFLIPobj.GeneralLogic/DomainObjects/Ports/ReadOnlyNSSFLIPobjRepositoryDecorator.cs
@@ -0,0 +1,35 @@
+﻿using NSSFLIPobj.DomainObjects;
+using NSSFLIPobj.DomainObjects.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSSFLIPobj.DomainObjects.Repositories
+{
+    public abstract class ReadOnlyNSSFLIPobjRepositoryDecorator : IReadOnlyNSSFLIPobjRepository
+    {
+        private readonly IReadOnlyNSSFLIPobjRepository _nssflipobjRepository;
+
+        public ReadOnlyNSSFLIPobjRepositoryDecorator(IReadOnlyNSSFLIPobjRepository nssflipobjRepository)
+        {
+            _nssflipobjRepository = nssflipobjRepository;
+        }
+
+        public virtual async Task<IEnumerable<nssflipobj>> GetAllNSSFLIPobjs()
+        {
+            return await _nssflipobjRepository?.GetAllNSSFLIPobjs();
+        }
+
+        public virtual async Task<nssflipobj> GetNSSFLIPobj(long id)
+        {
+            return await _nssflipobjRepository?.GetNSSFLIPobj(id);
+        }
+
+        public virtual async Task<IEnumerable<nssflipobj>> QueryNSSFLIPobjs(ICriteria<nssflipobj> criteria)
+        {
+            return await _nssflipobjRepository?.QueryNSSFLIPobjs(criteria);
+        }
+    }
+}
