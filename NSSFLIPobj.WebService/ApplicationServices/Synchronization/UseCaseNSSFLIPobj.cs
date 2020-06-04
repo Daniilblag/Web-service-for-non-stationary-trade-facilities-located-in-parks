11ffbf9b55af221e106cf6af8fb6f2083c1d3d5f
Daniil <51130836+Daniilblag@users.noreply.github.com>
2020-06-04 17:00:53 +0900

    Add files via upload

--- NSSFLIPobj.WebService/ApplicationServices/Synchronization/UseCaseNSSFLIPobj.cs
+++ NSSFLIPobj.WebService/ApplicationServices/Synchronization/UseCaseNSSFLIPobj.cs
@@ -5,6 +5,11 @@ using NSSFLIPobj.DomainObjects;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.IO;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using NSSFLIPobj.InfrastructureServices.Gateways.Database;
 
 namespace NSSFLIPobj.ApplicationServices.Synchronization
 {
@@ -12,7 +17,7 @@ namespace NSSFLIPobj.ApplicationServices.Synchronization
     {
         public int global_id { get; set; }
         public int Number { get; set; }
-        public NSSFLIPobj_inf Cells { get; set; }
+        public NSSFLIPobj_inf Cells { get; set; }   
     }
 
     public class NSSFLIPobj_inf
@@ -37,11 +42,12 @@ namespace NSSFLIPobj.ApplicationServices.Synchronization
         public string TradingStartDate { get; set; }
     }
 
-
     public class UseCaseNSSFLIPobj
     {
         static string remoteUri = "https://apidata.mos.ru/v1/datasets/61356/rows?api_key=aa284d4afd4fc64eab7198014a34847c";
 
+        string path = @".\update_database\nssflipobj-";
+        
         List<NSSFLIPobj_Cell> nssflipobj_cells;
 
         public List<nssflipobj> nssflipobjs = new List<nssflipobj>();
@@ -51,41 +57,72 @@ namespace NSSFLIPobj.ApplicationServices.Synchronization
             
             WebRequest request = WebRequest.Create(remoteUri);
             WebResponse response = request.GetResponse();
-
-            
+           
             StreamReader stream = new StreamReader(response.GetResponseStream());
             string line = stream.ReadToEnd();
-
+            
             JArray jsonArray = JArray.Parse(line);
-
+            
             nssflipobj_cells = JsonConvert.DeserializeObject<List<NSSFLIPobj_Cell>>(jsonArray.ToString());
+         
+            DateTime Date_update = DateTime.Now;
+            string date_update = Date_update.ToShortDateString();
 
-
-            for (int i = 0; i < nssflipobj_cells.Count; i++)
+            path = path + date_update + @".json";
+            
+            using (FileStream fs2 = new FileStream(path, FileMode.OpenOrCreate))
             {
-                nssflipobjs.Add(new nssflipobj()
+                
+                var options = new JsonSerializerOptions
                 {
-                    Number = nssflipobj_cells[i].Cells.Number,
-                    Name = nssflipobj_cells[i].Cells.Name,
-                    AdminArea = nssflipobj_cells[i].Cells.AdminArea,
-                    District = nssflipobj_cells[i].Cells.District,
-                    Adress = nssflipobj_cells[i].Cells.Adress,
-                    CodAdrIP = nssflipobj_cells[i].Cells.CodAdrIP,
-                    AreaObj = nssflipobj_cells[i].Cells.AreaObj,
-                    KindObj = nssflipobj_cells[i].Cells.KindObj,
-                    Specialization = nssflipobj_cells[i].Cells.Specialization,
-                    PlacementPeriod = nssflipobj_cells[i].Cells.PlacementPeriod,
-                    TypeOfContract = nssflipobj_cells[i].Cells.TypeOfContract,
-                    ContractStatus = nssflipobj_cells[i].Cells.ContractStatus,
-                    NumberOfContract = nssflipobj_cells[i].Cells.NumberOfContract,
-                    NameOfBusinessEntity = nssflipobj_cells[i].Cells.NameOfBusinessEntity,
-                    ContractStartDate = nssflipobj_cells[i].Cells.ContractStartDate,
-                    ContractFinishDate = nssflipobj_cells[i].Cells.ContractFinishDate,
-                    GroundsForConcluding = nssflipobj_cells[i].Cells.GroundsForConcluding,
-                    TradingStartDate = nssflipobj_cells[i].Cells.TradingStartDate
-                });
-            }
-        }
+                    Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                    WriteIndented = true
+                };
+                
+                string text = "[";
+                byte[] input = Encoding.Default.GetBytes(text);
+               
+                fs2.Write(input, 0, input.Length);
+                text = ",";
 
+                for (int i = 0; i < nssflipobj_cells.Count; i++)
+                {
+                    nssflipobjs.Add(new nssflipobj()
+                    {
+                        Number = nssflipobj_cells[i].Cells.Number,
+                        Name = nssflipobj_cells[i].Cells.Name,
+                        AdminArea = nssflipobj_cells[i].Cells.AdminArea,
+                        District = nssflipobj_cells[i].Cells.District,
+                        Adress = nssflipobj_cells[i].Cells.Adress,
+                        CodAdrIP = nssflipobj_cells[i].Cells.CodAdrIP,
+                        AreaObj = nssflipobj_cells[i].Cells.AreaObj,
+                        KindObj = nssflipobj_cells[i].Cells.KindObj,
+                        Specialization = nssflipobj_cells[i].Cells.Specialization,
+                        PlacementPeriod = nssflipobj_cells[i].Cells.PlacementPeriod,
+                        TypeOfContract = nssflipobj_cells[i].Cells.TypeOfContract,
+                        ContractStatus = nssflipobj_cells[i].Cells.ContractStatus,
+                        NumberOfContract = nssflipobj_cells[i].Cells.NumberOfContract,
+                        NameOfBusinessEntity = nssflipobj_cells[i].Cells.NameOfBusinessEntity,
+                        ContractStartDate = nssflipobj_cells[i].Cells.ContractStartDate,
+                        ContractFinishDate = nssflipobj_cells[i].Cells.ContractFinishDate,
+                        GroundsForConcluding = nssflipobj_cells[i].Cells.GroundsForConcluding,
+                        TradingStartDate = nssflipobj_cells[i].Cells.TradingStartDate
+                    });
+
+                    System.Text.Json.JsonSerializer.SerializeAsync<nssflipobj>(fs2, nssflipobjs[i], options).GetAwaiter().GetResult();
+
+                    if (i != nssflipobj_cells.Count - 1)
+                    {
+                        input = Encoding.Default.GetBytes(text);
+                        fs2.Write(input, 0, input.Length);
+                    }
+                }
+
+                text = "]";
+                input = Encoding.Default.GetBytes(text);
+                fs2.Write(input, 0, input.Length);
+            }   
+        }
     }
 }
+
